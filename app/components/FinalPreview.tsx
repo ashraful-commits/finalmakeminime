af1129cfeae66a8d67c984a4387a8a06bd1a0345
Md Ashraful alam <ashrafulalam10000@gmail.com>
2025-04-10 15:40:57 +0600

    update style

--- app/components/FinalPreview.tsx
+++ app/components/FinalPreview.tsx
@@ -16,8 +16,12 @@ const FinalPreview: React.FC<FinalPreviewProps> = ({
 }) => {
   const handleAddNewFace = () => {
     setStep(0);
-  };
 
+  };
+  const handleEdit = () => {
+    setStep(4);
+
+  };
   return (
     <div
       className={`lg:w-[50%] md:w-[50%]  max-sm:w-full max-sm:flex justify-between max-sm:justify-center lg:flex lg:flex-col md:flex md:flex-col lg:items-start md:items-start max-sm:items-center  space-y-4 min-h-full px-5 max-sm:mb-20 max-sm:gap-5 ${
@@ -26,8 +30,8 @@ const FinalPreview: React.FC<FinalPreviewProps> = ({
     >
       {croppedImage && (
         <div className="flex justify-start max-sm:justify-center items-center gap-3 max-sm:flex-col max-sm:gap-1">
-          <p className="text-blue-500 text-lg max-sm:text-sm text-start">Face</p>
-          <img className="w-20 h-auto max-sm:w-10 " src={croppedImage} alt="Cropped face" />
+          <p className="text-blue-500 text-lg max-sm:text-sm text-start max-sm:mt-3">Face</p>
+          <button className="cursor-pointer hover:outline-1" onClick={handleEdit}><img className="w-20 h-auto max-sm:w-10 " src={croppedImage} alt="Cropped face" /></button>
         </div>
       )}
       <div className="flex justify-start max-sm:justify-center items-center gap-3 max-sm:flex-col max-sm:gap-1">
